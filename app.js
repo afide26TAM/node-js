7064bb93dc8074f1e6a2bbd962a2695d7eaaefa5
afide26TAM <98856359+afide26TAM@users.noreply.github.com>
2022-04-28 17:52:41 +1000

    Use pug for HTML templates

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+// Views engine
+app.set("view engine", "pug");
+app.set("views", "views");
+
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const { engine } = require('express-handlebars');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
