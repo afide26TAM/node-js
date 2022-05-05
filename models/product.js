bf5d5eecc118355d626470b8b8766da47b053c24
afide26TAM <98856359+afide26TAM@users.noreply.github.com>
2022-05-05 17:25:57 +1000

    Refactoring the model

--- models/product.js
+++ models/product.js
@@ -1,7 +1,18 @@
 const fs = require('fs');
 const path = require('path');
+
 const pathDir = require('../util/path');
 const p = path.join(pathDir, 'data','products.json');//File name and path
+
+const getProductsFromFile = (cb)=>{
+  fs.readFile(p, (err,fileContent)=>{
+    if(err){
+      cb([]);
+    }else{
+      cb(JSON.parse(fileContent));
+    }
+  });
+}
 
 
 module.exports = class Product{
@@ -12,26 +23,17 @@ module.exports = class Product{
   
   // Create custom function for each instance of the Class
   save(){
-    fs.readFile(p, (err,fileContent)=>{
-      let products=[];
-      if(!err){
-        products = JSON.parse(fileContent);
-      }
+    getProductsFromFile(products=>{
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products),(err)=>{
-        console.log(err);
-      })
+      fs.writeFile(p, JSON.stringify(products), err=>{
+        console.log(err)
+      });
     });
   }
 
   //Add static to the custom function to call the function of the Class and not on the instance of the Class
   static fetchAll(cb){
-    fs.readFile(p, (err,fileContent)=>{
-      if(err){
-        cb([]);
-      }
-      cb(JSON.parse(fileContent));
-    });
+    getProductsFromFile(cb);
   }
 }
 
